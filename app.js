83cf74cf688234cf4181aeb133c6f88f3b3159b7
emrebulun <bulunemre@gmail.com>
2024-03-16 18:37:38 +0300

    updated

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const express = require('express');
 const pageRoute = require('./routes/pageRoute');
 
 const app = express();
+
 //Template Engine
-app.set("view engine", "ejs");
-//Middleware
-app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+//Middlewares
+app.use(express.static('public'));
 
 //Routes
-
-app.use('/', pageRoute );
+app.use('/', pageRoute);
 
 const port = 3000;
-
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
 });
