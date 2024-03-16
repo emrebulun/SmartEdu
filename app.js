6202924f3ac57b5f4edf018fe5c7481405a18820
emrebulun <bulunemre@gmail.com>
2024-03-16 17:44:10 +0300

    uptaded

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require("express");
 
 const app = express();
+//Template Engine
+app.set("view engine","ejs");
+ //Middleware
+ app.use(express.static("public"));
+ 
+//Routes
 
 app.get("/", (req, res) => {
-  res.send("INDEX SAYFASI");
+ res.status(200).render('index',{
+  page_name: "index"
+  });
 });
+app.get("/about", (req, res) => {
+  res.status(200).render('about',{
+    page_name: "about"
+  });
+ });
 
 const port = 3000;
 
