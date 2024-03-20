b4ca018586cfe7e53edabe9fd2879a0431b59f18
emrebulun <bulunemre@gmail.com>
2024-03-20 23:25:22 +0300

    dashboard page is added

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,7 +27,7 @@ exports.loginUser = async (req, res) => {
       const same = await bcrypt.compare(password, user.password);
       if (same) {
         req.session.userID = user._id;
-        res.status(200).redirect("/");
+        res.status(200).redirect("/users/dashboard");
         // res.status(200).send("You are logged in");
       } else {
         res.status(401).send("Invalid password");
@@ -46,5 +46,14 @@ exports.loginUser = async (req, res) => {
 exports.logoutUser = (req, res) => {
   req.session.destroy(() => {
     res.redirect("/");
+  });
+};
+
+
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({_id : req.session.userID}); 
+  res.status(200).render('dashboard', {
+    page_name: 'dashboard',
+    user,
   });
 };
