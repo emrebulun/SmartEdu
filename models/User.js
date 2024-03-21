8beef12db4da4fc3fc82bc88a857922657b0bffe
emrebulun <bulunemre@gmail.com>
2024-03-21 21:41:35 +0300

    User roles are added

--- models/User.js
+++ models/User.js
@@ -17,7 +17,12 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
-  
+  role :{
+  type: String,
+  enum:["student" ,"teacher" ,"admin"],
+  default: "student",
+  }
+
   
 });
  
