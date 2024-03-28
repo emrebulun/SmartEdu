46167af8d1c9608e7468a1ad41521b72e6b9af88
emrebulun <bulunemre@gmail.com>
2024-03-28 23:10:35 +0300

    user validation on login and register page

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const {validationResult} = require(`express-validator`);
 const User = require('../models/User');
 const Category = require('../models/Category');
 const Course = require('../models/Course');
@@ -9,14 +10,19 @@ exports.createUser = async (req, res) => {
 
     res.status(201).redirect('/login');
   } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      error,
-    });
-  }
-};
+   const errors = validationResult(req);
+   console.log(errors);
+   console.log(errors.array()[0].msg); 
+    
+   for (let i = 0; i < errors.array().length; i++) {
+    req.flash("error",`${errors.array()[i].msg}`);
+   }
+   
+   res.status(400).redirect('/register');
+  };
+  };
 
-exports.loginUser = async (req, res) => {
+  exports.loginUser = async (req, res) => {
   
     const { email, password } = req.body; 
     //Assuming you're sending email and password in the request body 
@@ -24,25 +30,29 @@ exports.loginUser = async (req, res) => {
       const user = await User.findOne({ email }); // Find the user by email 
 
       if (user) {
-      const same = await bcrypt.compare(password, user.password); // Compare passwords 
-      if (same) { // Create a user session or send a token 
+      const isSame= await bcrypt.compare(password, user.password); // Compare passwords 
+      // Create a user session or send a token 
       // res.status(200).send("You are logged in");
-      req.session.userID=user._id;
-      res.status(200).redirect('/users/dashboard'); 
-      } else {
-      res.status(401).send("Invalid password"); 
-      } } else { 
-        res.status(404).send("User not found");
-      }
+        if(isSame) {
+          req.session.userID=user._id;
+          res.status(200).redirect('/users/dashboard');
+        } else {
+        req.flash("error" , "Your password is not correct!");
+        res.status(400).redirect('/login');
+        }
+      }else {
+        req.flash("error" , "User is not exist!");
+        res.status(400).redirect('/login');
+      } 
       
-      } catch (error) {
+    }catch (error) {
       res.status(500).json({
         status: "error",
         error 
       }); 
-      } 
-  };
+  } 
 
+}
 exports.logoutUser = (req, res) => {
   req.session.destroy(()=> {
     res.redirect('/');
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,8 +2,23 @@ const express = require('express')
 const authController = require('../controllers/authController');
 const authMiddleware = require('../middlewares/authMiddleware');
 const router = express.Router();
+const {body} = require(`express-validator`);
+const User = require('../models/User');
 
-router.route('/signup').post(authController.createUser); // http://localhost:3000/users/signup
+router.route('/signup').post(
+    [
+        body('name').not().isEmpty().withMessage('Please Enter Your Name'),
+
+
+        body('email').isEmail().withMessage('Please Enter Valid Email')
+        .custom((userEmail)=> {
+            return User.findOne({email:userEmail}).then(user => {
+                if (user) {
+                    return Promise.reject('Email is already exists!')
+                }
+            })
+        }),
+],authController.createUser); // http://localhost:3000/users/signup
 router.route('/login').post(authController.loginUser);
 router.route('/logout').get(authController.logoutUser);
 router.route('/dashboard').get(authMiddleware, authController.getDashboardPage);
