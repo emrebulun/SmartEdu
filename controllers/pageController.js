ecb17cced616e3518e2f4947dfc150292629d422
emrebulun <bulunemre@gmail.com>
2024-03-30 22:18:52 +0300

    home page is edited

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,9 +1,24 @@
 const nodemailer = require('nodemailer');
+const Course = require('../models/Course');
 
-exports.getIndexPage = (req, res) => {
-  console.log(req.session.userID);
+const User = require('../models/User');
+
+
+exports.getIndexPage = async (req, res) => {
+
+  // console.log(req.session.userID);
+
+  const courses = await Course.find().sort('-createdAt').limit(2); // limit 2 sayesinde en son oluşturulmuş 2 kursu çağırmış olduk
+  const totalCourses = await Course.find().countDocuments();
+  const totalStudents = await User.countDocuments({role:'student'});
+  const totalTeachers = await User.countDocuments({role:'teacher'});
+
   res.status(200).render('index', {
     page_name: 'index',
+    courses,
+    totalCourses,
+    totalStudents,
+    totalTeachers,
   });
 };
 exports.getAboutPage = (req, res) => {
